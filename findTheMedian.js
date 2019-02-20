e60f3024b1d5087a60f9860bec7b0b99fb73c2d8
Jimmie <jtarrodean@gmail.com>
2019-02-20 12:19:22 -0800

    file commit

---
+++ findTheMedian.js
@@ -0,0 +1,37 @@
+function findTheMedian(arr){
+ let ans = arr.sort((a,b) => a - b);
+
+ if (ans.length % 2 === 0){
+    while(ans.length > 2){
+        ans.pop();
+        ans.shift();
+
+    }
+    ans.pop();
+    console.log(ans);
+    return ans;
+ }else{
+     while(ans.length > 1){
+        ans.pop();
+        ans.shift();
+     }
+ }
+ console.log(ans);
+ return ans
+}
+
+findTheMedian([2,4,7]);
+
+findTheMedian([22,25,45,42,23,54,543,433, 322, 322])
+
+findTheMedian([2,4,7,6]);
+
+findTheMedian([2,4,6,8,10])
+
+// if(number % 2 === 0){
+//     return "is even";
+// }else if(number == Math.round(number)){
+//     console.log("odd");
+// }else{
+//     console.log("invalid");
+// }
